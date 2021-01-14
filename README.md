bd92ad2c4b92eddb5bf471885e70ee3cfde7c3e9
Dustin Ray <dustinray.dev@gmail.com>
2021-01-14 18:58:22 -0500

    First Commit. Need to add timer and do some styling

---
+++ README.md
@@ -0,0 +1,7 @@
+#About
+
+I wanted to make a JavaScript BuzzFeed-Style quiz. I probably am providing waaaay to much time, but there are a lot of Pokémon and if someone letigimately can remember almost 900 of them, they deserve the chance to be able to finish.
+
+...also I just wanted to play it, so I made it. Whatever.
+
+Pokemon names and images retrieved via the Poke API: https://pokeapi.co/
---
+++ script.js
@@ -0,0 +1,73 @@
+const startBtn = document.querySelector(".start");
+const input = document.querySelector(".guess-input");
+const identified = document.querySelector("#correct");
+const pokemonNames = [];
+const guessed = [];
+
+startBtn.disabled = true;
+
+getPokemonNames();
+
+async function getPokemonNames() {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=898");
+    const data = await res.json();
+    populateArray(data.results);
+}
+
+function addToList(id) {
+    const pokemon = document.createElement("div");
+    pokemon.classList.add("identified");
+    pokemon.innerHTML = `
+        <div class="div-sprite">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id + 1}.png" alt="">
+        </div>
+        <div class="circle"></div>`;
+    identified.appendChild(pokemon);
+}
+
+function updateScroll() {
+    identified.scrollTop = identified.scrollHeight;
+}
+
+function populateArray(array) {
+    for (let i = 0; i < array.length; i++) {
+        if(i === 121 || i === 438 || i === 865) {
+            switch (i) {
+                case 121: pokemonNames.push("mr. mime");
+                break;
+                case 438: pokemonNames.push("mime jr.");
+                break;
+                case 865: pokemonNames.push("mr. rime");
+                break;
+            }
+        } else {
+            pokemonNames.push(array[i].name.toLowerCase());
+        }
+    }
+    console.log(pokemonNames);
+    console.log(pokemonNames[121], pokemonNames[438], pokemonNames[865]);
+    startBtn.disabled = false;
+}
+
+function checkName(name) {
+    if (pokemonNames.includes(name)) {
+        const index = pokemonNames.indexOf(name);
+        guessed.push(name);
+        addToList(index);
+        return true;
+    }
+    return false;
+}
+
+startBtn.addEventListener('click', () => {
+    startBtn.parentElement.classList.add("hidden");
+});
+
+input.addEventListener('input', (e) => {
+    const inputText = e.target.value.toLowerCase();
+    if (!guessed.includes(inputText)) {
+        if(checkName(inputText)) {
+            input.value = "";
+        }
+    }
+});
